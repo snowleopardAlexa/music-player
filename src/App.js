96a8a1c97952fe85c2eded1acc59a4ae676c46a2
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-29 19:15:01 -0500

    add directory to img and songs

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ function App() {
      {
        title: "I Want To Break Free",
        artist: "Queen",
-       img_src: "breakfree.jpg",
-       src: "BreakFree.m4a"
+       img_src: "./img/breakfree.jpg",
+       src: "./songs/BreakFree.m4a"
      },
      {
       title: "Show Must Go On",
       artist: "Queen",
-      img_src: "breakfree.jpg",
-      src: "BreakFree.m4a"
+      img_src: "./img/breakfree.jpg",
+      src: "./songs/BreakFree.m4a"
     },
     {
       title: "Breakthrough",
       artist: "Queen",
-      img_src: "breakfree.jpg",
-      src: "BreakFree.m4a"
+      img_src: "./img/breakfree.jpg",
+      src: "./songs/BreakFree.m4a"
     },
     {
       title: "Radio Gaga",
       artist: "Queen",
-      img_src: "breakfree.jpg",
-      src: "BreakFree.m4a"
+      img_src: "./img/breakfree.jpg",
+      src: "./songs/BreakFree.m4a"
     },
   ])
 
