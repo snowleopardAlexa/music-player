c82059714e28771515294671ebe5170725a911b1
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-02 22:10:38 -0500

    add details

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Details from './Details';
 
 function Player() {
     return (
         <div className="player">
-            Hello from player.
+            <h4>Playing Now</h4>
+            <Details />
         </div>
-    )
+    );
 }
 
 export default Player
